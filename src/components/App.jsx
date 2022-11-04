6854e0912d8701bbfcf00125facb78e66b4a3471
Volodymyr Husak <volodyaog2907@gmail.com>
2022-11-04 22:06:32 +0200

    delete classes and add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-
-import { Component } from 'react';
+// import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ThreeCircles } from 'react-loader-spinner';
@@ -8,137 +7,271 @@ import { SearchBar } from './SearchBar/SearchBar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    imageName: '',
-    arrImage: [],
-    page: 1,
-    isLoading: false,
-    isModal: null,
+export const App = () => {
+  const [imageName, setImageName] = useState('');
+  const [arrImage, setArrImage] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModal, setIsModal] = useState(null);
+
+  const KEY = '29882224-53e6cb6eb5c61ad27904c20c4';
+
+  useEffect(() => {
+    window.addEventListener('keydown', onPressEsc);
+    return () => {
+      window.removeEventListener('keydown', onPressEsc);
+    };
+  }, [isModal]);
+
+  // const arrImageForFetch = arrImage;
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      `https://pixabay.com/api/?q=${imageName}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(res => res.json())
+      .then(obj => {
+        if (arrImage.length > 0) {
+          // console.log('dsfsdfdsfs')
+          setIsLoading(false);
+          setArrImage(prevState => {
+            return [...prevState, ...obj.hits];
+          });
+        } else {
+        setPage(1);
+        setArrImage(obj.hits);
+        setIsLoading(false);
+        }
+      });
+  }, [imageName, page]);
+
+  // const addObjectsImage = arr => {
+  //   if (arrImage.length > 0) {
+  //     console.log('dsfsdfdsfs');
+  //     setIsLoading(false);
+  //     setArrImage(prevState => {
+  //       return [...prevState, ...arr];
+  //     });
+  //   }
+  // };
+
+  const handleFormSubmit = name => {
+    setPage(1);
+    setArrImage([]);
+    setImageName(name);
   };
 
-  KEY = '29882224-53e6cb6eb5c61ad27904c20c4';
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.isModal) {
-      window.addEventListener('keydown', this.onPressEsc);
-    }
-    if (this.state.isModal === null) {
-      window.removeEventListener('keydown', this.onPressEsc);
-    }
-
-    if (
-      prevState.imageName !== this.state.imageName ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ isLoading: true });
-      fetch(
-        `https://pixabay.com/api/?q=${this.state.imageName}&page=${this.state.page}&key=${this.KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then(res => res.json())
-        .then(obj => {
-          if (this.state.arrImage.length > 0) {
-            this.setState({ isLoading: false });
-            this.setState(prevState => {
-              return { arrImage: [...prevState.arrImage, ...obj.hits] };
-            });
-          } else {
-            this.setState({ page: 1 });
-            this.setState({ arrImage: obj.hits });
-            this.setState({ isLoading: false });
-          }
-        });
-    }
-  }
-
-  handleFormSubmit = name => {
-    this.setState({ page: 1 });
-    this.setState({ arrImage: [] });
-    this.setState({ imageName: name });    
-  };
-
-  onClickLoadMore = e => {
+  const onClickLoadMore = e => {
     e.preventDefault();
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
+    setPage(prevState => {
+      return prevState + 1;
     });
   };
 
-  onClickImage = e => {
-      const currentModalId = Number(e.currentTarget.id);
-      this.state.arrImage.map(obj => {
-        if (obj.id === currentModalId) {
-          return this.setState({ isModal: obj });
-        } else {
-          return null;
-        }
-      });
+  const onClickImage = e => {
+    const currentModalId = Number(e.currentTarget.id);
+    arrImage.map(obj => {
+      if (obj.id === currentModalId) {
+        return setIsModal(obj);
+      } else {
+        return null;
+      }
+    });
   };
 
-  closeModalClickBackdrop = e => {
+  const closeModalClickBackdrop = e => {
     // console.log(e.currentTarget);
     // if (e.currentTarget.className === 'Overlay') {
-      this.setState({ isModal: null });
+    setIsModal(null);
     // }
   };
 
-  onPressEsc = e => {
+  const onPressEsc = e => {
     if (e.key === 'Escape') {
       // console.log(e.key)
-      this.setState({ isModal: null });
+      setIsModal(null);
       window.removeEventListener('keydown', this.onWindowKeypress);
     }
   };
 
-  render() {
-    return (
-      <div
-        // onKeyDown={this.closeModalEsc}
-        style={{
-          height: '100vh',
-          // display: 'flex',
-          // justifyContent: 'center',
-          // alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <SearchBar onFormSubmit={this.handleFormSubmit} />
+  return (
+    <div
+      // onKeyDown={this.closeModalEsc}
+      style={{
+        height: '100vh',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <SearchBar
+        onFormSubmit={handleFormSubmit}
+        setImageName={setImageName}
+        imageName={imageName} />
 
-        <ImageGallery
-          arrImage={this.state.arrImage}
-          onClickImage={this.onClickImage}
-        />
-        {this.state.isLoading && (
-          <ThreeCircles
-            position="bottom-center"
-            height="100"
-            width="100"
-            color="#35a089"
-            wrapperStyle={{}}
-            wrapperClass="spinner"
-            visible={true}
-            ariaLabel="three-circles-rotating"
-            outerCircleColor=""
-            innerCircleColor=""
-            middleCircleColor=""
-          />
-        )}
-        {this.state.arrImage.length > 0 && (
-          <Button onClickLoadMore={this.onClickLoadMore} />
-        )}
-        {this.state.isModal && (
-          <Modal
-            currentImage={this.state.isModal}
-            closeModalClickBackdrop={this.closeModalClickBackdrop}
-          />
-        )}
-        <ToastContainer
+      <ImageGallery arrImage={arrImage} onClickImage={onClickImage} />
+      {isLoading && (
+        <ThreeCircles
           position="bottom-center"
-          autoClose={2000}
-          theme="dark"
+          height="100"
+          width="100"
+          color="#35a089"
+          wrapperStyle={{}}
+          wrapperClass="spinner"
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor=""
+          innerCircleColor=""
+          middleCircleColor=""
         />
-      </div>
-    );
-  }
-}
+      )}
+      {arrImage.length > 0 && <Button onClickLoadMore={onClickLoadMore} />}
+      {isModal && (
+        <Modal
+          currentImage={isModal}
+          closeModalClickBackdrop={closeModalClickBackdrop}
+        />
+      )}
+      <ToastContainer position="bottom-center" autoClose={2000} theme="dark" />
+    </div>
+  );
+};
+
+// export class App1 extends Component {
+//   state = {
+//     imageName: '',
+//     arrImage: [],
+//     page: 1,
+//     isLoading: false,
+//     isModal: null,
+//   };
+
+//   KEY = '29882224-53e6cb6eb5c61ad27904c20c4';
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.isModal) {
+//       window.addEventListener('keydown', this.onPressEsc);
+//     }
+//     if (this.state.isModal === null) {
+//       window.removeEventListener('keydown', this.onPressEsc);
+//     }
+
+//     if (
+//       prevState.imageName !== this.state.imageName ||
+//       prevState.page !== this.state.page
+//     ) {
+//       this.setState({ isLoading: true });
+//       fetch(
+//         `https://pixabay.com/api/?q=${this.state.imageName}&page=${this.state.page}&key=${this.KEY}&image_type=photo&orientation=horizontal&per_page=12`
+//       )
+//         .then(res => res.json())
+//         .then(obj => {
+//           if (this.state.arrImage.length > 0) {
+//             this.setState({ isLoading: false });
+//             this.setState(prevState => {
+//               return { arrImage: [...prevState.arrImage, ...obj.hits] };
+//             });
+//           } else {
+//             this.setState({ page: 1 });
+//             this.setState({ arrImage: obj.hits });
+//             this.setState({ isLoading: false });
+//           }
+//         });
+//     }
+//   }
+
+//   handleFormSubmit = name => {
+//     this.setState({ page: 1 });
+//     this.setState({ arrImage: [] });
+//     this.setState({ imageName: name });
+//   };
+
+//   onClickLoadMore = e => {
+//     e.preventDefault();
+//     this.setState(prevState => {
+//       return { page: prevState.page + 1 };
+//     });
+//   };
+
+//   onClickImage = e => {
+//     const currentModalId = Number(e.currentTarget.id);
+//     this.state.arrImage.map(obj => {
+//       if (obj.id === currentModalId) {
+//         return this.setState({ isModal: obj });
+//       } else {
+//         return null;
+//       }
+//     });
+//   };
+
+//   closeModalClickBackdrop = e => {
+//     // console.log(e.currentTarget);
+//     // if (e.currentTarget.className === 'Overlay') {
+//     this.setState({ isModal: null });
+//     // }
+//   };
+
+//   onPressEsc = e => {
+//     if (e.key === 'Escape') {
+//       // console.log(e.key)
+//       this.setState({ isModal: null });
+//       window.removeEventListener('keydown', this.onWindowKeypress);
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div
+//         // onKeyDown={this.closeModalEsc}
+//         style={{
+//           height: '100vh',
+//           // display: 'flex',
+//           // justifyContent: 'center',
+//           // alignItems: 'center',
+//           fontSize: 40,
+//           color: '#010101',
+//         }}
+//       >
+//         <SearchBar onFormSubmit={this.handleFormSubmit} />
+
+//         <ImageGallery
+//           arrImage={this.state.arrImage}
+//           onClickImage={this.onClickImage}
+//         />
+//         {this.state.isLoading && (
+//           <ThreeCircles
+//             position="bottom-center"
+//             height="100"
+//             width="100"
+//             color="#35a089"
+//             wrapperStyle={{}}
+//             wrapperClass="spinner"
+//             visible={true}
+//             ariaLabel="three-circles-rotating"
+//             outerCircleColor=""
+//             innerCircleColor=""
+//             middleCircleColor=""
+//           />
+//         )}
+//         {this.state.arrImage.length > 0 && (
+//           <Button onClickLoadMore={this.onClickLoadMore} />
+//         )}
+//         {this.state.isModal && (
+//           <Modal
+//             currentImage={this.state.isModal}
+//             closeModalClickBackdrop={this.closeModalClickBackdrop}
+//           />
+//         )}
+//         <ToastContainer
+//           position="bottom-center"
+//           autoClose={2000}
+//           theme="dark"
+//         />
+//       </div>
+//     );
+//   }
+// }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,54 +1,103 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
-import propTypes from 'prop-types'; 
-import css from './SearchBar.module.css'
+import propTypes from 'prop-types';
+import css from './SearchBar.module.css';
 
-export class SearchBar extends Component {
-  state = {
-    imageName: '',
-  };
+export const SearchBar = ({
+  onFormSubmit,
+  // setImageName,
+  // imageName
+}) => {
+  const [imageName, setImageName] = useState();
 
-  handleImageNameChange = e => {
+  const handleImageNameChange = e => {
     // console.log(e.currentTarget.value);
-    this.setState({ imageName: e.currentTarget.value });
-    e.currentTarget.value = this.state.imageName;
+    setImageName(e.currentTarget.value);
+    e.currentTarget.value = imageName;
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.imageName.trim() === '') {
-        toast.warn("Введіть назву зображення");
+    if (imageName.trim() === '') {
+      toast.warn('Введіть назву зображення');
       return;
     }
     // this.setState({ imageName: e.target.inputImage.value });
-    this.props.onFormSubmit(this.state.imageName);
+    onFormSubmit(imageName);
     // e.target.inputImage.value = '';
-    this.setState({ imageName: '' });
+    setImageName('');
   };
 
-  render() {
-    return (
-      <header className={css.searchBar}>
-        <form className={css.searchForm} onSubmit={this.handleSubmit} >
-          <button className={css.searchForm_button} type="submit" >
-            <span className={css.searchForm_button_label}>Search</span>
-          </button>
+  return (
+    <header className={css.searchBar}>
+      <form className={css.searchForm} onSubmit={handleSubmit}>
+        <button className={css.searchForm_button} type="submit">
+          <span className={css.searchForm_button_label}>Search</span>
+        </button>
 
-          <input className={css.searchForm_input}
-            onChange={this.handleImageNameChange}
-            name="inputImage"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.imageName}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.searchForm_input}
+          onChange={handleImageNameChange}
+          name="inputImage"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={imageName}
+        />
+      </form>
+    </header>
+  );
+};
 
 SearchBar.propTypes = {
   onFormSubmit: propTypes.func.isRequired,
 };
+
+// export class SearchBar2 extends Component {
+//   state = {
+//     imageName: '',
+//   };
+
+//   handleImageNameChange = e => {
+//     // console.log(e.currentTarget.value);
+//     this.setState({ imageName: e.currentTarget.value });
+//     e.currentTarget.value = this.state.imageName;
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     if (this.state.imageName.trim() === '') {
+//       toast.warn('Введіть назву зображення');
+//       return;
+//     }
+//     // this.setState({ imageName: e.target.inputImage.value });
+//     this.props.onFormSubmit(this.state.imageName);
+//     // e.target.inputImage.value = '';
+//     this.setState({ imageName: '' });
+//   };
+
+//   render() {
+//     return (
+//       <header className={css.searchBar}>
+//         <form className={css.searchForm} onSubmit={this.handleSubmit}>
+//           <button className={css.searchForm_button} type="submit">
+//             <span className={css.searchForm_button_label}>Search</span>
+//           </button>
+
+//           <input
+//             className={css.searchForm_input}
+//             onChange={this.handleImageNameChange}
+//             name="inputImage"
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.imageName}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
